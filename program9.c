b2e933bf6c94453eac553029caa66ff29473ea92
abhay-byte <abhay02delhi@gmail.com>
2023-11-27 19:03:57 +0530

    program 9 Updated.

--- program9.c
+++ program9.c
@@ -1,34 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-struct Student
-{
+struct Student{
     char name[50];
     int rollNo;
     struct Student *next;
 };
-void InsertNode(char name[50], int rollNo, struct Student** start, int pos)
-{
+void InsertNode(char name[50], int rollNo, struct Student** start, int pos){
     struct Student* ele = (struct Student*)malloc(sizeof(struct Student));
     strcpy(ele->name, name);
     ele->rollNo = rollNo;
     ele->next = NULL;
-    if (*start == NULL)
-        *start = ele;
-    else
-    {
-        if (pos == -1)
-        {
+    if (*start == NULL) *start = ele;
+    else{
+        if (pos == -1){
             struct Student* ele_next = *start;
-            while (ele_next->next != NULL)
-            {
-                ele_next = ele_next->next;
-            }
+            while (ele_next->next != NULL) ele_next = ele_next->next;
             ele_next->next = ele;
             printf("\nNode Added with name %s.", name);
         }
-        else
-        {
+        else{
             int currPos = 1;
             struct Student* ele_next = *start;
             while (ele_next != NULL && pos != currPos)
@@ -36,31 +27,22 @@ void InsertNode(char name[50], int rollNo, struct Student** start, int pos)
                 ele_next = ele_next->next;
                 currPos++;
             }
-
-            if (pos == currPos)
-            {
+            if (pos == currPos){
                 struct Student* temp = ele_next->next;
                 ele_next->next = ele;
                 ele->next = temp;
                 printf("\nNode Added with name %s.", name);
                 printf("\nAt position %i.", pos);
             }
-            else
-            {
-                printf("\nPosition not found.");
-            }
+            else printf("\nPosition not found.");
         }
     }
 }
-void DeleteNodeWithRollNumber(struct Student** start, int rollNo)
-{
+void DeleteNodeWithRollNumber(struct Student** start, int rollNo){
     struct Student* ele_next = (*start)->next;
     struct Student* prev = *start;
-
-    while (ele_next != NULL)
-    {
-        if (rollNo == ele_next->rollNo)
-        {
+    while (ele_next != NULL){
+        if (rollNo == ele_next->rollNo){
             prev->next = ele_next->next;
             printf("\nNode Deleted.");
             free(ele_next);
@@ -71,32 +53,25 @@ void DeleteNodeWithRollNumber(struct Student** start, int rollNo)
     }
     printf("\nNode to be Deleted not found.");
 }
-void ReverseLinkedList(struct Student** start)
-{
+void ReverseLinkedList(struct Student** start){
     struct Student* prev = NULL;
     struct Student* current = *start;
     struct Student* nextNode = NULL;
-
-    while (current != NULL)
-    {
+    while (current != NULL){
         nextNode = current->next;
         current->next = prev;
         prev = current;
         current = nextNode;
     }
-
     *start = prev;
     printf("\nLinked List Reversed.");
 }
-void main()
-{
+void main(){
     struct Student* start = NULL;
-
     InsertNode("ABC", 1, &start,-1);
     InsertNode("EFG", 2, &start,-1);
     InsertNode("HIK", 3, &start,-1);
     InsertNode("IJK", 4, &start, 2);
-
     DeleteNodeWithRollNumber(&start, 4);
     ReverseLinkedList(&start);
     printf("\nAbhay Raj, 00976803122");
